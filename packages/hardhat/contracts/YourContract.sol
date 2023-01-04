6ce9b49972623aa84da6d7a28cf6825a2e3fe532
Mugrebot <mikeongay@icloud.com>
2023-01-04 15:23:39 -0500

    optimized gas

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -12,114 +12,103 @@ pragma solidity >=0.8.0 <0.9.0;
 
 
 
-//start writing the contract
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract YourContract {
-    //create a struct that will hold the subscription tier information
 
-    address private owner;
+contract YourContract is Ownable {
+    using SafeMath for uint256;
 
-    struct SubscriptionTier {
-        uint256 subscriptionFee;
-        uint256 subscriptionDuration;
-    }
-    
-    //create a struct that will hold the user information
-    struct User {
-        address userAddress;
-        uint256 subscriptionTier;
-        uint256 subscriptionExpiration;
-    }
-    
-    //create a mapping that will hold the subscription tier information
-    mapping(uint256 => SubscriptionTier) public subscriptionTiers;
-    
-    //create a mapping that will hold the user information
-    mapping(address => User) public users;
-    
-    //create an event that will be emitted when a user pays for a subscription
-    event UserSubscribed(address userAddress, uint256 subscriptionTier, uint256 subscriptionExpiration);
-    
-    //create an event that will be emitted when the owner withdraws the funds from the contract
-    event FundsWithdrawn(address owner, uint256 amount);
-    
-    //create an event that will be emitted when the owner changes the subscription fee for a tier
-    event SubscriptionFeeChanged(uint256 subscriptionTier, uint256 subscriptionFee);
-    
-    //create an event that will be emitted when the owner changes the subscription duration for a tier
-    event SubscriptionDurationChanged(uint256 subscriptionTier, uint256 subscriptionDuration);
-    
-    //create a constructor that will initialize the subscription tiers
+//set the owner of the contract to 0x30fA487ee1b335b5f76aF590f2452d83BaAA71fC
     constructor() {
-        owner=0x72d5fCC549C7454F2017Fe8ad19EdD800aB7EB92;
-        subscriptionTiers[1] = SubscriptionTier(.05 ether, 30 days);
-        subscriptionTiers[2] = SubscriptionTier(.25 ether, 180 days);
-        subscriptionTiers[3] = SubscriptionTier(.45 ether, 365 days);
-    }
-    
-    //create a function that will allow the user to pay the subscription fee and will give them the subscription tier they paid for
-    function subscribe(uint256 subscriptionTier) public payable {
-        require(msg.value == subscriptionTiers[subscriptionTier].subscriptionFee, "You must pay the correct subscription fee");
-        require(subscriptionTier >= 1 && subscriptionTier <= 3, "You must choose a valid subscription tier");
-        
- 
-        users[msg.sender].subscriptionExpiration += subscriptionTiers[subscriptionTier].subscriptionDuration;
-        
-        users[msg.sender].userAddress = msg.sender;
-        users[msg.sender].subscriptionTier = subscriptionTier;
-
-        
-        emit UserSubscribed(msg.sender, subscriptionTier, users[msg.sender].subscriptionExpiration);
-    }
-    
-    //create a read only function that will allow anyone to check a user's subscription status
-    function checkSubscriptionStatus(address userAddress) public view returns(uint256) {
-
-            return users[userAddress].subscriptionExpiration;
-        
+        transferOwnership(0x30fA487ee1b335b5f76aF590f2452d83BaAA71fC);
     }
 
-    //create a read onlyfunction that returns a boolean value if the user is subscribed or not
+    struct Subscription {
+        uint256 tier;
+        uint256 expiration;
+    }
 
-    function isSubscribed(address userAddress) public view returns(bool) {
-        if(users[userAddress].subscriptionExpiration > 0) {
-            return true;
+    mapping(address => Subscription) public subscriptions;
+
+    uint256 public tier1Price = .05 ether;
+    uint256 public tier2Price = .25 ether;
+    uint256 public tier3Price = .45 ether;
+
+    uint256 public tier1Duration = 30 days;
+    uint256 public tier2Duration = 180 days;
+    uint256 public tier3Duration = 365 days;
+
+    function subscribe(uint256 _tier) public payable {
+        require(_tier == 1 || _tier == 2 || _tier == 3, "Invalid tier");
+        require(
+            msg.value == tier1Price && _tier == 1 ||
+            msg.value == tier2Price && _tier == 2 ||
+            msg.value == tier3Price && _tier == 3,
+            "Invalid price"
+        );
+
+        subscriptions[msg.sender] = Subscription(_tier, block.timestamp.add(
+            _tier == 1 ? tier1Duration :
+            _tier == 2 ? tier2Duration :
+            tier3Duration
+        ));
+    }
+
+
+    //payable receive function that will allow the user to pay the subscription fee and will give them the subscription tier they paid for
+    //reverts if not enough ether sent, returns any extra ether sent
+    receive() external payable {
+        require(msg.value > 0, "No ether sent");
+        if (msg.value >= tier3Price) {
+            subscriptions[msg.sender] = Subscription(3, block.timestamp.add(tier3Duration));
+            payable(msg.sender).transfer(msg.value.sub(tier3Price));
+        } else if (msg.value >= tier2Price) {
+            subscriptions[msg.sender] = Subscription(2, block.timestamp.add(tier2Duration));
+            payable(msg.sender).transfer(msg.value.sub(tier2Price));
+        } else if (msg.value >= tier1Price) {
+            subscriptions[msg.sender] = Subscription(3, block.timestamp.add(tier1Duration));
+            payable(msg.sender).transfer(msg.value.sub(tier1Price));
         } else {
-            return false;
+            revert("Not enough ether sent");
         }
     }
-    
-    //create a function that will allow the owner to withdraw the funds from the contract
-    function withdrawFunds() public {
-        require(msg.sender == owner, "You must be the owner to withdraw the funds");
-        
-        uint256 balance = address(this).balance;
-        payable(msg.sender).transfer(balance);
-        
-        emit FundsWithdrawn(msg.sender, balance);
+
+
+
+    function isSubscribed(address _user) public view returns (bool) {
+        return subscriptions[_user].expiration > block.timestamp;
     }
-    
-    //create a function that will allow the owner to change the subscription fee for each tier
-    function changeSubscriptionFee(uint256 subscriptionTier, uint256 subscriptionFee) public {
-        require(msg.sender == owner, "You must be the owner to change the subscription fee");
-        require(subscriptionTier >= 1 && subscriptionTier <= 3, "You must choose a valid subscription tier");
-        
-        subscriptionTiers[subscriptionTier].subscriptionFee = subscriptionFee;
-        
-        emit SubscriptionFeeChanged(subscriptionTier, subscriptionFee);
+
+
+    function withdraw() public onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
     }
-    
-    //create a function that will allow the owner to change the subscription duration for each tier
-    function changeSubscriptionDuration(uint256 subscriptionTier, uint256 subscriptionDuration) public {
-        require(msg.sender == owner, "You must be the owner to change the subscription duration");
-        require(subscriptionTier >= 1 && subscriptionTier <= 3, "You must choose a valid subscription tier");
-        
-        subscriptionTiers[subscriptionTier].subscriptionDuration = subscriptionDuration;
-        
-        emit SubscriptionDurationChanged(subscriptionTier, subscriptionDuration);
+
+    function setTier1Price(uint256 _price) public onlyOwner {
+        tier1Price = _price;
+    }
+
+    function setTier2Price(uint256 _price) public onlyOwner {
+        tier2Price = _price;
+    }
+
+    function setTier3Price(uint256 _price) public onlyOwner {
+        tier3Price = _price;
+    }
+
+    function setTier1Duration(uint256 _duration) public onlyOwner {
+        tier1Duration = _duration;
+    }
+
+    function setTier2Duration(uint256 _duration) public onlyOwner {
+        tier2Duration = _duration;
+    }
+
+    function setTier3Duration(uint256 _duration) public onlyOwner {
+        tier3Duration = _duration;
     }
 }
 
-//end writing the contract
 
 
